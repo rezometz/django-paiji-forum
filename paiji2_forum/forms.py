ec6e40c530b5c22ca9ae0b710eae80f77e0939e6
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2016-09-27 23:23:35 +0200

    Revert "Merge pull request #13 from rezometz/placeholder-alternative"
    This reverts commit f1224456ed031f57f833e7193bf937c60b19c40e, reversing
    changes made to 74b850ad0ed4b5d518382ae2546609e0f32dc52d.

--- paiji2_forum/forms.py
+++ paiji2_forum/forms.py
@@ -57,6 +57,10 @@ class Markdownarea(Textarea):
 class AnswerForm(ModelForm):
 
     text = CharField(
+        initial=_('''\
+Use the *Markdown* format here.
+
+Cf. [documentation](http://daringfireball.net/projects/markdown/basics).'''),
         widget=Markdownarea,
         strip=False,
         min_length=5,
